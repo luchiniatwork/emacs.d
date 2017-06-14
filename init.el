93722fa8285e6b3f8ea04171260af0e7d647da4e
Tiago Luchini <luchini@work.co>
2017-06-14 09:49:32 -0400

    Adds several packages (may be a dependency for latest commit)

--- init.el
+++ init.el
@@ -170,15 +170,23 @@
  '(coffee-tab-width 2)
  '(custom-safe-themes
    (quote
-    ("c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
+    ("9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" "c7a9a68bd07e38620a5508fef62ec079d274475c8f92d75ed0c33c45fbe306bc" default)))
  '(git-gutter:added-sign "+")
  '(git-gutter:deleted-sign "-")
  '(git-gutter:hide-gutter t)
  '(git-gutter:modified-sign "*")
  '(git-gutter:update-interval 2)
+ '(guide-key-mode t)
+ '(guide-key/popup-window-position (quote bottom))
+ '(org-agenda-files (quote ("~/.notes")))
  '(package-selected-packages
    (quote
-    (monokai-theme git-gutter markdown-mode smooth-scrolling ac-cider popup auto-complete tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider))))
+    (dockerfile-mode window-number dot-mode graphviz-dot-mode geeknote guide-key "guide-key" "discover-my-major" eshell-git-prompt docker company monokai-theme git-gutter markdown-mode smooth-scrolling ac-cider popup auto-complete tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider)))
+ '(safe-local-variable-values
+   (quote
+    ((cider-cljs-lein-repl . "(do (user/run) (user/browser-repl))")
+     (cider-refresh-after-fn . "reloaded.repl/resume")
+     (cider-refresh-before-fn . "reloaded.repl/suspend")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
