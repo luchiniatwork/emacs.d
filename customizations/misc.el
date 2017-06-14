44f3bf9c3778956f2d26eb3956938d5efa22fd30
Tiago Luchini <luchini@work.co>
2017-06-14 09:46:30 -0400

    Adding key bindings for magit

--- customizations/misc.el
+++ customizations/misc.el
@@ -10,3 +10,17 @@
 
 ;; Go straight to scratch buffer on startup
 (setq inhibit-startup-message t)
+
+;; Global keybindings
+
+;; Magit
+(global-set-key (kbd "C-c g s") 'magit-status)
+(global-set-key (kbd "C-c g b") 'magit-show-refs-popup)
+(global-set-key (kbd "C-c g l") 'magit-log-all-branches)
+(global-set-key (kbd "C-c g c") 'magit-checkout)
+(global-set-key (kbd "C-c g h") 'magit-branch-and-checkout)
+(global-set-key (kbd "C-c g p") 'magit-pull-popup)
+(global-set-key (kbd "C-c g r") 'magit-rebase-interactive)
+
+;; Start a new eshell even if one is active.
+(global-set-key (kbd "C-c s s") (lambda () (interactive) (eshell t)))
